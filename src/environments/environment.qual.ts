be4c0b1a2e0507afa42f4b6f5b9ad72849e20df5
Joris-G <66126635+Joris-G@users.noreply.github.com>
2022-05-30 17:13:31 +0200

    V1.4.3 Qual

---
+++ src/environments/environment.qual.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiServer: 'http://152.60.136.236',
+  moldingApi: ':85/api/',
+  toolApi: ':86/api/',
+  usineApi: ':87/api/',
+  username: '204292',
+  password: 'test'
+};
