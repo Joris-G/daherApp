07225bfbdded8ed977a6b5aefeebb085ebb0208c
Joris-G <66126635+Joris-G@users.noreply.github.com>
2022-06-20 13:41:55 +0200

    env add tool-api-dev
    Signed-off-by: Joris-G <66126635+Joris-G@users.noreply.github.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServer: 'http://152.60.136.236:81/',
   moldingApi: 'Tracakit-api-dev/public/index.php/api/',
-  toolApi: 'outillage-api/public/index.php/api/',
+  toolApi: 'outillage-api-dev/public/index.php/api/',
   usineApi: 'usine-api-dev/public/index.php/api/',
   username: '204292',
   password: 'test'
