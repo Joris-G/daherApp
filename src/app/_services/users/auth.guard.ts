d4dc02abeb103dc269832f19d9d2c10bffe0addb
Joris-G <66126635+Joris-G@users.noreply.github.com>
2022-06-29 09:51:17 +0200

    authGuard
    Signed-off-by: Joris-G <66126635+Joris-G@users.noreply.github.com>

--- src/app/_services/users/auth.guard.ts
+++ src/app/_services/users/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class AuthGuard implements CanActivate {
     private router: Router,
     private authService: AuthService) { }
   canActivate() {
-    if (this.authService.isAuth) {
+    if (this.authService.isAuth || environment.name === 'DEV' || environment.name === 'QUAL') {
       return true;
     } else {
       this.router.navigate(['/login']);
