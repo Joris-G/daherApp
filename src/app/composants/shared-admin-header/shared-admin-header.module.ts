37868852362d7e2c8af05ef0b7262f700de7e054
Joris-G <66126635+Joris-G@users.noreply.github.com>
2022-06-30 09:07:04 +0200

    indentation du code
    Signed-off-by: Joris-G <66126635+Joris-G@users.noreply.github.com>

--- src/app/composants/shared-admin-header/shared-admin-header.module.ts
+++ src/app/composants/shared-admin-header/shared-admin-header.module.ts
@@ -5,8 +5,16 @@ import { IonicModule } from '@ionic/angular';
 import { SharedAdminHeaderComponent } from './shared-admin-header.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule,],
-  declarations: [SharedAdminHeaderComponent],
-  exports: [SharedAdminHeaderComponent]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+  ],
+  declarations: [
+    SharedAdminHeaderComponent
+  ],
+  exports: [
+    SharedAdminHeaderComponent
+  ]
 })
 export class SharedAdminHeaderModule { }
