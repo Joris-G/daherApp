a34c1d7f5d1304e80d2a97c8bb5418aca36ff5d7
Joris-G <66126635+Joris-G@users.noreply.github.com>
2022-07-06 14:55:32 +0200

    Directive mise en forme kit périmé
    Signed-off-by: Joris-G <66126635+Joris-G@users.noreply.github.com>

--- src/app/_directives/directive.module.ts
+++ src/app/_directives/directive.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PerempDirective } from './peremp.directive';
 
 @NgModule({
   imports: [
@@ -9,6 +10,10 @@ import { CommonModule } from '@angular/common';
 
   ],
   exports: [
+
+  ],
+  providers: [
+
   ]
 })
 
---
+++ src/app/_directives/peremp.directive.spec.ts
@@ -0,0 +1,8 @@
+import { PerempDirective } from './peremp.directive';
+
+describe('PerempDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PerempDirective();
+    expect(directive).toBeTruthy();
+  });
+});
