d80839eb20d39cefeebf49b4d9cc13a60cb4a501
Joris-G <66126635+Joris-G@users.noreply.github.com>
2022-12-19 14:44:07 +0100

    build prod qual V1.6.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   name: '',
-  apiServer: 'http://152.60.136.236:81/',
+  apiServer: 'http://localhost/',
   moldingApi: 'Tracakit-api/public/index.php/api/',
   toolApi: 'outillage-api/public/index.php/api/',
   usineApi: 'usine-api/public/index.php/api/',
--- src/environments/environment.qual.ts
+++ src/environments/environment.qual.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   name: 'QUAL',
-  apiServer: 'http://152.60.136.236:81/',
+  apiServer: 'http://localhost/',
   moldingApi: 'Tracakit-api-dev/public/index.php/api/',
   toolApi: 'outillage-api-dev/public/index.php/api/',
   usineApi: 'usine-api-dev/public/index.php/api/',
