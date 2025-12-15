e1df7455a6e3a196f011f7323ffc15d2e84f5b24
Joris-G <66126635+Joris-G@users.noreply.github.com>
2025-12-15 12:34:18 +0100

    Update mockUser.mock.ts
    remove Daher domain

--- src/mocks/mockData/mockUser.mock.ts
+++ src/mocks/mockData/mockUser.mock.ts
@@ -1,8 +1,8 @@
 import { User } from "src/app/_interfaces/user";
 
 export const mockUsers: User[] = [
-  { id: 1, isActive: false, nom: 'admin', prenom: 'admin', password: ' ', matricule: 111, email: 'admin@daher.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null },
-  { id: 2, isActive: true, nom: 'tooling', prenom: 'operateur', matricule: 112, email: 'tooling.operateur@daher.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null, password: 'test' },
-  { id: 3, isActive: true, nom: 'tooling', prenom: "chef d'équipe", matricule: 113, email: 'tooling.chef@daher.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null, password: 'test' },
-  { id: 4, isActive: true, nom: 'methode', prenom: 'operateur', matricule: 114, email: 'methode.operateur@daher.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null, password: 'test' },
+  { id: 1, isActive: false, nom: 'admin', prenom: 'admin', password: ' ', matricule: 111, email: 'admin@test.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null },
+  { id: 2, isActive: true, nom: 'tooling', prenom: 'operateur', matricule: 112, email: 'tooling.operateur@test.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null, password: 'test' },
+  { id: 3, isActive: true, nom: 'tooling', prenom: "chef d'équipe", matricule: 113, email: 'tooling.chef@test.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null, password: 'test' },
+  { id: 4, isActive: true, nom: 'methode', prenom: 'operateur', matricule: 114, email: 'methode.operateur@test.com', roles: [], createdAt: new Date(2025, 11, 3), lastCon: new Date(), poste: null, groupeAffected: [], programmeAvion: [], service: null, site: null, tel: ['1324'], unite: null, password: 'test' },
 ];
